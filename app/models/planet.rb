55959f47203f16b7aa8f47a26a1fb3f600dc7110
Matthieu Assie <matthieu.assie@gmail.com>
2021-08-18 15:26:30 +0200

    user story

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -1,4 +1,5 @@
 class Planet < ApplicationRecord
   belongs_to :user
+  has_many :bookings
   validates :name, :rotation_period, :diameter, :climate, :gravity, :terrain, :surface_water, :population, :price, presence: true
 end

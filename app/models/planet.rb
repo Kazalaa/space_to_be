48c42887b6b7290376723a1cc2f4ca669d7b5036
Matthieu Assie <matthieu.assie@gmail.com>
2021-08-19 11:30:18 +0200

    searcg index

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -3,4 +3,11 @@ class Planet < ApplicationRecord
   belongs_to :user
   has_many :bookings
   validates :name, :rotation_period, :diameter, :climate, :gravity, :terrain, :surface_water, :population, :price, presence: true
+
+  include PgSearch::Model
+  pg_search_scope :search_by_name_and_terrain,
+    against: [ :name, :terrain ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end

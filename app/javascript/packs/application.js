2c0948df1379ea98396186c7000c5aaecfd9e4ca
Pierre Cloute Cazalaa <pie.clouteca@gmail.com>
2021-08-19 14:55:38 +0200

    homepage done, need search form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,8 +27,9 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import { centerplanet } from '../plugins/planet_carrousel';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
-  // initSelect2();
+  centerplanet();
 });
